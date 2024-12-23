53a3a1911e70abcb479b761c451e7330e2830000
andreesantt <andresanttdev@gmail.com>
2024-12-23 19:32:49 -0300

    Introdução a JS
    Operadores

---
+++ aula-10/index.js
@@ -0,0 +1,13 @@
+let nome = "André"
+
+console.log(nome) // André
+console.error(nome)
+console.warn(nome)
+
+
+window.alert(nome)
+/* nome = window.prompt("Digite seu nome")
+let confirmacao = window.confirm("Você deseja continuar?")
+*/
+
+document.write(nome)    
