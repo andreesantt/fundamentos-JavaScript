7f348411e97440b5a83421d3de08475856144374
andreesantt <andresanttdev@gmail.com>
2024-12-24 08:59:51 -0300

    Calculadora de 4 Operações
    Programa em JS que lê dois número, guarda-os e exibe as quatro operações da matemática:
    ° Soma
    ° Subtração
    ° Multiplicação
    ° Divisão

---
+++ exercicio-2-Calculadora/index.js
@@ -0,0 +1,12 @@
+let primeiro_valor = prompt("Digite o primeiro valor: ");   // Recebe o primeiro valor
+let segundo_valor = prompt("Digite o segundo valor: ");     // Recebe o segundo valor
+
+// Soma o primeiro_valor com o segundo_valor
+let soma = parseInt(primeiro_valor) + parseInt(segundo_valor);
+alert(`A soma dos valores é: ${soma}`);// Exibe o resultado da soma
+let subtracao = parseInt(primeiro_valor) - parseInt(segundo_valor);
+alert(`A subtração dos valores é: ${subtracao}`)  ; // Exibe o resultado da subtração
+let multiplicacao = parseInt(primeiro_valor) * parseInt(segundo_valor);
+alert (`A multiplicação dos valores é: ${multiplicacao}`) ; // Exibe o resultado da multiplicação
+let divisao = parseInt(primeiro_valor) / parseInt(segundo_valor);
+alert (`A divisão dos valores é: ${divisao}`) ; // Exibe o resultado da divisão
