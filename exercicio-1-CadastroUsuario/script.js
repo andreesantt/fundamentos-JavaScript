ba65bf06cb28029a5da0c3e840c97148c1bc9c36
andreesantt <andresanttdev@gmail.com>
2024-12-24 08:05:23 -0300

    Exercício 1 - Cadastro de Recrutas
    Programa em JS que lê e armazena dados do usuário inseridos

---
+++ exercicio-1-CadastroUsuario/script.js
@@ -0,0 +1,9 @@
+let nome = prompt("Digite seu nome: ");   // Exibe um prompt para o usuário digitar seu nome
+
+let sobrenome = prompt("Digite seu sobrenome: ");   // Exibe um prompt para o usuário digitar seu sobrenome
+
+let campo_de_estudo = prompt('Qual seu campo de estudo?');   // Exibe um prompt para o usuário digitar seu campo de estudo
+
+let campo_estudo = prompt('Qual seu ano de nascimento?');   // Exibe um prompt para o usuário digitar seu ano de nascimento
+
+alert("Olá, " + nome + " " + sobrenome + "! Seja Bem-vindo(a)") // Exibe um alerta com a mensagem de boas-vindas
