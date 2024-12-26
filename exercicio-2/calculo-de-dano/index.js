29c6abeab979bb60b6a9e931f845ba2066a39951
andreesantt <andresanttdev@gmail.com>
2024-12-26 17:16:13 -0300

    Parte 2 - Exercicio 02 - Cálculo de Dano
    Inicio do código da segunda parte do segundo exercício do módulo de JS

---
+++ exercicio-2/calculo-de-dano/index.js
@@ -0,0 +1,7 @@
+const character_1 = prompt("Digite o nome do primeiro personagem: "); // Recebe o nome do primeiro personagem
+const damage_1 = parseFloat(prompt("Digite o dano do primeiro personagem: ")); // Recebe o dano do primeiro personagem
+
+const character_2 = prompt("Digite o nome do segundo personagem: "); // Recebe o nome do segundo personagem
+const damage_2 = parseFloat(prompt("Digite o dano do segundo personagem: "); // Recebe o dano do segundo personagem
+
+
