137e71e04e489fba9dcabb63a1340cd9f8f9635a
andreesantt <andresanttdev@gmail.com>
2024-12-27 20:45:33 -0300

    Exercicio 2 - Dano causado

--- exercicio-2/calculo-de-dano/index.js
+++ exercicio-2/calculo-de-dano/index.js
@@ -1,7 +1,31 @@
-const character_1 = prompt("Digite o nome do primeiro personagem: "); // Recebe o nome do primeiro personagem
+// Crie um programa que receba o nome de dois personagens e seus respectivos poderes de ataque.
+ alert("Vamos batalhar?"); // Alerta que a batalha vai começar
+// Informações do primeiro personagem:
+const attacker = prompt("Digite o nome do primeiro personagem: "); // Recebe o nome do primeiro personagem
 const damage_1 = parseFloat(prompt("Digite o dano do primeiro personagem: ")); // Recebe o dano do primeiro personagem
 
-const character_2 = prompt("Digite o nome do segundo personagem: "); // Recebe o nome do segundo personagem
-const damage_2 = parseFloat(prompt("Digite o dano do segundo personagem: "); // Recebe o dano do segundo personagem
+// Informações do segundo personagem:
+const defender = prompt("Digite o nome do segundo personagem: "); // Recebe o nome do segundo personagem
+let hp_defender= parseFloat(prompt("Digite o HP do segundo personagem: ")); // Recebe o nível de vida do segundo personagem
+const defenser_power = parseFloat(prompt("Digite o poder de defesa do segundo personagem: ")); // Recebe o poder de defesa do segundo personagem
+const shield_defender = prompt("O segundo personagem possui escudo? (Sim/Não) "); // Recebe se o segundo personagem possui escudo
+ let hp_final = 0;
 
+alert("Chegou a hora de batalhar!!!"); // Alerta que a batalha vai começar
+
+// Calcula o dano
+
+if (damage_1 > defenser_power && shield_defender === "Não") {
+    const damage1 = damage_1 - defenser_power;
+} else if (damage_1 > defenser_power && shield_defender === "Sim") {
+    const damage2 = (damage_1 - defender_power) / 2;
+}
+
+// Aplicação de dano
+
+hp_defender -= hp_final
+
+// Exibição do resultado
+
+alert("O dano causado pelo personagem " + attacker + " foi de " + damage1 + " pontos de vida. O personagem " + defender + " agora possui " + hp_defender_final + " pontos de vida.");
 
