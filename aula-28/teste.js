5ed5c1d510734e6749f43dc3026a787972c7ec0e
andreesantt <andresanttdev@gmail.com>
2025-01-01 23:47:29 -0300

    Mais testes com FOR

--- aula-28/teste.js
+++ aula-28/teste.js
@@ -1,6 +1,6 @@
-let name = 'André'
-let i = 5; // Define the variable i
+let count = 0
 
-for (let j = 0; j < i; j++) { // Use a different variable name inside the loop
-  alert(`Olá, ${i}!`)
+for (let i = 0; i < 100; i++) {
+  count += i
+  alert(count)
 }
