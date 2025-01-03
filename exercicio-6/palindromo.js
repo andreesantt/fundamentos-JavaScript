fdb1bfadfbeae84f9bea28848410d5e47530f8d1
andreesantt <andresanttdev@gmail.com>
2025-01-02 23:16:54 -0300

    Ex06.2 - Palindromo
    Outra forma de verificar se  apalavra é um palíndromo

--- exercicio-6/palindromo.js
+++ exercicio-6/palindromo.js
@@ -1,9 +1,24 @@
-let palindromo = prompt("Digite uma palavra para verificar se é palíndromo: ")
+// let palindromo = prompt("Digite uma palavra para verificar se é palíndromo: ")
 
-let palindromoinvertido = palindromo.split("").reverse("").join("")
+// let palindromoinvertido = palindromo.split("").reverse("").join("")
 
-if (palindromo === palindromoinvertido) {
-    alert("A palavra é um palíndromo")
-} else {
-    alert(`a palavra ${palindromo} invertida resulta em ${palindromoinvertido}, portanto não é um palíndromo`)
+// if (palindromo === palindromoinvertido) {
+//     alert("A palavra é um palíndromo")
+// } else {
+//     alert(`a palavra ${palindromo} invertida resulta em ${palindromoinvertido}, portanto não é um palíndromo`)
+// }
+
+//outra forma de fazer este mesmo exercício
+
+const word = prompt("Digite uma palavra para verificar se é palíndromo: ")
+let reverseWord = ""
+// macarrão
+for (let i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i]
+}
+
+if (word === reverseWord) {
+    alert(`A palavra ${word} é um palíndromo`)
+}   else {
+    alert(`a palavra ${word} invertida resulta em ${reverseWord}, portanto não é um palíndromo`)
 }
