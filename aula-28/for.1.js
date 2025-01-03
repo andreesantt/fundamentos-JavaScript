dbbffa3b6be7c3513bea2fd236ccdd52c1e8e4fd
andreesantt <andresanttdev@gmail.com>
2025-01-02 22:01:14 -0300

    Outras formas de sintase FOR

---
+++ aula-28/for.1.js
@@ -0,0 +1,10 @@
+// Inicia ; condiciona ; finaliza
+let indice = 0
+
+for ("Inicializando o FOR";
+    indice <= 10;
+    alert("Finalizando um bloco de repetição")
+) {
+    alert("O valor do indice é: " + indice)
+    indice++
+}
