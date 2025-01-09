5926e4913a20c19d436a721dd6d368f9becf3aed
andreesantt <andresanttdev@gmail.com>
2025-01-08 22:40:13 -0300

    Matrizes

--- aula-36/matrizes.js
+++ aula-36/matrizes.js
@@ -1,13 +1,20 @@
 const arr = [
-   ["1° Nível",
+   [ "1° Nível",
     "2° Nível",
     "3° Nível"
 ], [ "1° Nível",
     "2° Nível",
     "3° Nível"
+], [
+    "1° Nível",
+    "2° Nível",
+    "3° Nível"
+], [
+    "1° Nível",
+    "2° Nível",
+    "3° Nível"
 ]
     
-]
 ]
 
 console.log(arr)
