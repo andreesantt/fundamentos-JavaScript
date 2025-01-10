61cc9e4b64e5df3f1d897612eb644171497529fa
andreesantt <andresanttdev@gmail.com>
2025-01-10 20:40:31 -0300

    Exercicio 8 - Cadastro de Imoveis primeira parte

---
+++ exercicio-8/cadastro-imoveis.js
@@ -0,0 +1,24 @@
+const imoveis = []
+
+let opcao = ""
+do {
+    opcao = prompt( "Bem-vindo ao Cadastro de Imóveis.\nTotal Imóveis: " + imoveis.length +
+        "1. Cadastrar Imóveis" +
+        "\n2. Remover Imóveis" +
+        "\n3. Consultar Imóveis" +
+        "\n4. Sair"
+    )
+    switch (opcao) {
+        case '1':
+
+            break 
+        case "1":
+            
+            break
+
+        case "1":
+        
+            break
+        default:
+    } 
+} while (opcao !== "3");
