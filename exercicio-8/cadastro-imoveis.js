d265365ba0e333a5f78f4f6d0819e9d7a64742fd
andreesantt <andresanttdev@gmail.com>
2025-01-10 20:47:29 -0300

    Exercicio 8 - Dando opção de escolha do usuário

--- exercicio-8/cadastro-imoveis.js
+++ exercicio-8/cadastro-imoveis.js
@@ -1,24 +1,29 @@
 const imoveis = []
-
 let opcao = ""
+
 do {
-    opcao = prompt( "Bem-vindo ao Cadastro de Imóveis.\nTotal Imóveis: " + imoveis.length +
+    opcao = prompt( "Bem-vindo ao Cadastro de Imóveis.\nTotal Imóveis: " + imoveis.length + "\nEscolha uma opção: " +
         "1. Cadastrar Imóveis" +
-        "\n2. Remover Imóveis" +
-        "\n3. Consultar Imóveis" +
-        "\n4. Sair"
+        "\n2. Listar Imoveis" +
+        "\n3. Sair"
     )
     switch (opcao) {
         case '1':
 
             break 
         case "1":
-            
+            const imovel = {}
+
+            imovel.propietario = prompt("Informe o nome do propieário do imóvel: ")
+            imovel.quartos = prompt('Quantos quartos possui o imóvel: ')
+            imovel.banheiros = prompt('Quantos banheiros possui o imóvel: ')
+            imovel.garagem = prompt('O imóvel possui garagem? (Sim/Não)')
             break
 
         case "1":
-        
+            alert('Encerrando...')
             break
         default:
+            alert("Opção inválida!")
     } 
 } while (opcao !== "3");
