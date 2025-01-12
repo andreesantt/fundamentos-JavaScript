057b1804e94c91487163aa28d4726af7f3edf4f1
andreesantt <andresanttdev@gmail.com>
2025-01-12 11:57:58 -0300

    Com VAR há casos que dá para acessar o escopo das partes mais internas

--- aula-44/escopo.js
+++ aula-44/escopo.js
@@ -12,5 +12,37 @@ function criarAnimal() {
     let animal = "gato"
 }
 
-criarAnimal()
-console.log(animal)
+// criarAnimal()
+// console.log(animal)
+
+function avaliarNota(nota) {
+    if (nota > 60) {
+        var aprovado = true
+        let situacao = "Aprovado"
+    } else {
+        var aprovado = false
+        let situacao = "Reprovado"
+    }
+
+    console.log(nota)
+    console.log(aprovado)   
+    console.log(situacao)
+}
+
+// avaliarNota(83)
+// avaliarNota(49)
+
+function ola() {
+    var texto = "Olá, Mundo!"
+}
+
+// console.log(texto)
+
+console.log(nome)
+// console.log(sobrenome)
+
+var nome = "André"
+let sobrenome = "André"
+
+console.log(nome)
+// console.log(sobrenome)
