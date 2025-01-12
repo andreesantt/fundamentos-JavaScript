41d6e03ba1d20273ca0513a5ab4b7ccd0c6a0079
andreesantt <andresanttdev@gmail.com>
2025-01-12 11:25:45 -0300

    criando objetos dentro das funções e usando o retorno

--- aula-43/retorno.js
+++ aula-43/retorno.js
@@ -6,3 +6,17 @@ function calcularMedia(a, b) {
 const resultado = calcularMedia(7, 2)
 
 console.log(resultado)
+
+
+function criarProduto(nome, preco) {
+    const produto = {
+        nome, // nome: nome
+        preco, // preco: preco
+        estoque: 1
+    }
+    return produto
+}
+
+const notebook = criarProduto("Notebook Intel Core i3 8gb", 2500)
+
+console.log(notebook)
