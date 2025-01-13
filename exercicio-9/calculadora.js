40e2fe07342c762022c0e8067fdaafd33cb01fa3
andreesantt <andresanttdev@gmail.com>
2025-01-13 09:58:27 -0300

    Ex009 - Comentários no código
    Comentários para que quem leia possa o identificar onde foi feito cada coisa

--- exercicio-9/calculadora.js
+++ exercicio-9/calculadora.js
@@ -1,29 +1,29 @@
-function calcular_area_triangulo(base, altura) {
+function calcular_area_triangulo(base, altura) { // função que calcula a área do triângulo
     let areatriagulo =  (base * altura) / 2
     alert('Área do triângulo: ' + areatriagulo)
 }
 
-function calcular_area_retangulo(base, altura) {
+function calcular_area_retangulo(base, altura) { // função que calcula a área do retângulo
      let arearetangulo = base * altura
      alert('Área do retângulo: ' + arearetangulo)
 }
 
-function calcular_area_quadrado(lado) {
+function calcular_area_quadrado(lado) { // função que calcula a área do quadrado
     let areaquadrado = lado ** 2
     alert('Área do quadrado: ' + areaquadrado)
 }
 
-function calcular_area_trapezio(basemaior, basemenor, altura) {
+function calcular_area_trapezio(basemaior, basemenor, altura) { // função que calcula a área do trapézio
     let areatrapezio = ((basemaior + basemenor) * altura) / 2
     alert('Área do trapézio: ' + areatrapezio)
 }
 
-function calcular_area_circulo(raio, Pi = 3.14 ) {
+function calcular_area_circulo(raio, Pi = 3.14 ) { // função que calcula a área do círculo
     let areacirculo =  Pi * raio ** 2
     alert('Área do círculo: ' + areacirculo)
 }
 
-function menuPrincipal() {
+function menuPrincipal() { // função que exibe o menu, faz o prompt e guarda informação do usuário, loopa e faz cado caso de acordo com escolha do usuário
     let opcao;
     do {
         opcao = prompt(
@@ -70,4 +70,4 @@ function menuPrincipal() {
     } while (opcao !== '6')
 }
 
-menuPrincipal()
+menuPrincipal() // Chama a função para que seja exibida na tela
