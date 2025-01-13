b3651d86d77d16e4916cd0e78e58728fd9f2a564
andreesantt <andresanttdev@gmail.com>
2025-01-12 23:43:40 -0300

    Exercico 9 - Parte 2
    Do while com menu interativo e switch com opções

--- exercicio-9/calculadora.js
+++ exercicio-9/calculadora.js
@@ -18,17 +18,49 @@ function calcular_area_circulo(raio, Pi = 3.14 ) {
     return Pi * raio ** 2
 }
 
-function menuPrincipal(menu) {
+function menuPrincipal() {
     let opcao;
     do {
-        alert(
+        opcao = prompt(
             "Bem-vindo a Calculadora Geométrica." +
             "\nEscolha uma das opçoes abaixo:\n" +
             "\n1. Calcular área do triângulo" +
             "\n2. Calcular área do retângulo" +
-            "\n3. Calcular área do trapézio" +
-            "\n4. Calcular área do circulo"
-        )
+            "\n3. Calcular área do quadrado" +
+            "\n4. Calcular área do trapézio" +
+            "\n5. Calcular área do círculo" +
+            "\n6. Sair"
+        );
+        switch(opcao) {
+            case "1":
+                let base = parseFloat(prompt("Digite a base do triângulo:"));
+                let altura = parseFloat(prompt("Digite a altura do triângulo:"));
+                calcular_area_triangulo(base, altura);
+                break
+            case "2":
+                let baseRetangulo = parseFloat(prompt("Digite a base do retângulo:"));
+                let alturaRetangulo = parseFloat(prompt("Digite a altura do retângulo:"));
+                calcular_area_retangulo(baseRetangulo, alturaRetangulo);
+                break
+            case "3":
+                let lado = parseFloat(prompt("Digite o lado do quadrado:"));
+                calcular_area_quadrado(lado);
+                break
+            case "4":
+                let baseMaior = parseFloat(prompt("Digite a base maior do trapézio:"));
+                let baseMenor = parseFloat(prompt("Digite a base menor do trapézio:"));
+                let alturaTrapezio = parseFloat(prompt("Digite a altura do trapézio:"));
+                calcular_area_trapezio(baseMaior, baseMenor, alturaTrapezio);
+                break;
+            case "5":
+                let raio = parseFloat(prompt("Digite o raio do círculo:"));
+                calcular_area_circulo(raio);
+            case "6":
+                alert('Finalizando...')
+                break
+            default:
+                alert('Opção Inválida!')
+        }
     } while (opcao !== '6')
 }
 
