1e4d58ade7d1eeb298b81a3afe8906c147bdcd52
andreesantt <andresanttdev@gmail.com>
2025-01-13 15:20:32 -0300

    Comportamento da função recursiva

--- aula-48/recursao.js
+++ aula-48/recursao.js
@@ -7,6 +7,18 @@ function dividir(num) {
     }
 }
 
-dividir(256)
+// dividir(256)
 
 // Primeira -> Segunda -> Terceira -> Quarta
+
+function dobrar(num) {
+    console.log(num)
+    dobrar(num * 2)
+}
+
+// dobrar(0)
+
+// Fatorial de 5: !5 === 5 * 4 * 3 * 2 * 1 === 5 * !4
+function fatorial(num) {
+    
+}
