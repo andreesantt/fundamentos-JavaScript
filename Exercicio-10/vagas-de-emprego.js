693b2d7bb91eeefdd3d907e68013df56092b8f76
andreesantt <andresanttdev@gmail.com>
2025-01-15 16:02:23 -0300

    Exercicio 10 - Criação do Array vagas e uso do for na função listar vagas

--- Exercicio-10/vagas-de-emprego.js
+++ Exercicio-10/vagas-de-emprego.js
@@ -1,8 +1,32 @@
 let opcao = ""
-let vaga_de_emprego = []
+let vaga_de_emprego = [
+    {
+        nome: "Desenvolvedor Frontend",
+        descricao: "Vaga para desenvolvedor frontend com experiência em React.",
+        data_limite: "2025-01-30",
+        candidatos: ["João", "Maria"],
+    },
+    {
+        nome: "Analista de Sistemas",
+        descricao: "Vaga para analista de sistemas com experiência em Python.",
+        data_limite: "2025-02-15",
+        candidatos: ["Pedro"],
+    },
+    {
+        nome: "Designer Gráfico",
+        descricao: "Vaga para designer gráfico com conhecimento em Adobe Illustrator.",
+        data_limite: "2025-01-20",
+        candidatos: ["Ana", "Lucas", "Fernanda"],
+    }
+]
 
 function listar_vagas() {
-    
+   function listar_vagas() {
+    let vaga = vaga_de_emprego[i]; // Obtém cada vaga pelo índice
+        alert(`Índice: ${i}\nNome: ${vaga.nome}\nCandidatos inscritos: ${vaga.candidatos.length}`);
+    }
+}
+
 }
 
 function criar_nova_vaga() {
