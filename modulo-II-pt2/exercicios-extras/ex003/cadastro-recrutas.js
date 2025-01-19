e7a38ebee2b8cf4a516f7b59e00715aac40f0728
andreesantt <andresanttdev@gmail.com>
2025-01-19 16:28:19 -0300

    Ex004 - Refazendo exercício básico Cadastro de Usuário

---
+++ modulo-II-pt2/exercicios-extras/ex003/cadastro-recrutas.js
@@ -0,0 +1,6 @@
+const nome = prompt('Digite seu nome: ')
+const sobrenome=  prompt('Digite seu sobrenome: ')
+const campoDeEstudo = prompt('Digite o campo de estudo: ')
+const anoNascimento = prompt('Digite seu ano de nascimento: ')
+
+alert('Olá ' + nome + ' ' + sobrenome + '. você faz a ' + campoDeEstudo + ' e tem ' + (2025 - anoNascimento) + ' anos de idade')
