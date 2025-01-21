9a723215c9c216d22a60e7b3ca7cdd161b0d037b
andreesantt <andresanttdev@gmail.com>
2025-01-20 21:57:57 -0300

    Ex005 - Usando Modularização, resolvendo de outra forma
    Uso de funções para fazer de outra forma e melhorar a legibilidade, reuso, e manutenção do código

--- modulo-II-pt2/exercicios-extras/ex005/divisivel.js
+++ modulo-II-pt2/exercicios-extras/ex005/divisivel.js
@@ -1,14 +1,28 @@
+// const numberOne = 5
+// const numberTwo = 10
+
+// if (numberOne % 2 === 0) {
+//     console.log('O número ' + numberOne + ' é divisível por 2 portanto é um número par')
+// } else {
+//     console.log('Não é um número par')
+// }
+
+// if (numberTwo % 2 === 0) {
+//     console.log('O número ' + numberTwo + ' é divisível por 2 portanto é um número par')
+// } else {
+//     console.log('Não é um número par')
+// }
+
+function verificarParidade(numero) {
+    if (numero % 2 === 0) {
+        console.log(`O número ${numero} é divsível por 2, portanto é um número par`)
+    } else {
+        console.log(`O número ${numero} não é par`)
+    }
+}
+
 const numberOne = 5
 const numberTwo = 10
 
-if (numberOne % 2 === 0) {
-    console.log('O número ' + numberOne + ' é divisível por 2 portanto é um número par')
-} else {
-    console.log('Não é um número par')
-}
-
-if (numberTwo % 2 === 0) {
-    console.log('O número ' + numberTwo + ' é divisível por 2 portanto é um número par')
-} else {
-    console.log('Não é um número par')
-}
+verificarParidade(numberOne)
+verificarParidade(numberTwo)
