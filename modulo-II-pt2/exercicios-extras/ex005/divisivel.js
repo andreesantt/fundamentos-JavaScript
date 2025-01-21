525e8b380547000d5696149d661036b85a18b2ef
andreesantt <andresanttdev@gmail.com>
2025-01-20 21:51:13 -0300

    Ex005 - Verificando se o número é par

---
+++ modulo-II-pt2/exercicios-extras/ex005/divisivel.js
@@ -0,0 +1,14 @@
+const numberOne = 5
+const numberTwo = 10
+
+if (numberOne % 2 === 0) {
+    console.log('O número ' + numberOne + ' é divisível por 2 portanto é um número par')
+} else {
+    console.log('Não é um número par')
+}
+
+if (numberTwo % 2 === 0) {
+    console.log('O número ' + numberTwo + ' é divisível por 2 portanto é um número par')
+} else {
+    console.log('Não é um número par')
+}
